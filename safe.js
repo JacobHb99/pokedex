cd456eb4817412842b4d08c95391edb939863184
JacobHb99 <jacob.hb@live.de>
2023-05-11 17:02:56 +0200

    renderDatas

---
+++ safe.js
@@ -0,0 +1,7 @@
+<div id="cardImgContainer">
+<h2 class="card-title">${currentPokemon['name']}</h2>
+<img class="pokemonImg" src="${currentPokemon['sprites']['front_shiny']}" alt="">
+<h6 class="card-subtitle mb-2 text-body-secondary">${currentPokemon['types']['0']['type']['name']}</h6>
+<h6 class="card-subtitle mb-2 text-body-secondary">${currentPokemon['weight']}kg</h6>
+
+</div>
